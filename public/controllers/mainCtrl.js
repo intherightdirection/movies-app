955406317030c53eeebf36c687e47ddbb83b5b35
intherightdirection <jdunlop81@gmail.com>
2019-01-19 21:56:52 -0500

    Made each movie in its own JSON file

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,9 +1,10 @@
 angular.module('movieApp', ['ngRoute'])
 .config(($routeProvider) => {
+  //determines which html view will be displayed depending on address in address bar
   $routeProvider.when('/surprise/:playId', { templateUrl: '/views/movie-view.html' });
   $routeProvider.otherwise({ templateUrl: '/views/movie-info.html' });
 })
-  .constant('dataUrl', 'http://localhost:3000/movies.json')
+  .constant('dataUrl', 'http://localhost:3000/movies.json') //sets constant dataUrl
   .controller('mainController', function($scope, $http, dataUrl) {
     $scope.data = {};
 
